dd5a3ed37295538255b5346f95b9916416f3ff4d
lbilinmis <lbilinmis@gelisim.edu.tr>
2023-03-12 15:09:12 +0300

    Add project files.

---
+++ Ajax.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Ajax.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using static Ajax.WebUI.Models.Kullanici;
+
+namespace Ajax.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult GetList()
+        {
+            Thread.Sleep(5000);
+            var jsonKullanicilar = JsonConvert.SerializeObject(KullaniciIslem.GetKullanicilar());
+
+            return Json(jsonKullanicilar);
+        }
+
+        public IActionResult GetById(int id)
+        {
+            var kullanici = KullaniciIslem.GetKullanici(id);
+            var jsonKullanici = JsonConvert.SerializeObject(kullanici);
+
+            return Json(jsonKullanici);
+        }
+
+
+        [HttpPost]
+        public IActionResult KullaniciEkle(Kullanici entity)
+        {
+            KullaniciIslem.Insert(entity);
+            var jsonKullanici = JsonConvert.SerializeObject(entity);
+
+            return Json(jsonKullanici);
+        }
+    }
+}
---
+++ Ajax.WebUI/Models/Kullanici.cs
@@ -0,0 +1,47 @@
+﻿using Ajax.WebUI.Models;
+
+namespace Ajax.WebUI.Models
+{
+    public class Kullanici
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        public static class KullaniciIslem
+        {
+            private static  List<Kullanici> kullanicilar = GetKullanicilar();
+
+            public static Kullanici GetKullanici(int id)
+            {
+                return kullanicilar.FirstOrDefault(x => x.Id == id);
+            }
+            public static void Insert(Kullanici entity)
+            {
+                 kullanicilar.Add(entity);
+            }
+
+            public static List<Kullanici> GetKullanicilar()
+            {
+                List<Kullanici> list = new List<Kullanici>();
+
+                for (int i = 0; i < 20; i++)
+                {
+                    Kullanici kullanici = new Kullanici();
+                    kullanici.Name = FakeData.NameData.GetFullName();
+                    kullanici.Id = i + 1;
+                    list.Add(kullanici);
+                }
+
+                return list;
+            }
+
+        }
+
+
+
+      
+
+    }
+
+}
